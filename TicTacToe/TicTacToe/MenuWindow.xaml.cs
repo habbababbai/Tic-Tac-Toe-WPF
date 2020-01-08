b9b86bad5a801d2b82b670264a85ab1bc9566fed
DarSpace <45310234+DarSpace@users.noreply.github.com>
2020-01-08 23:55:53 +0100

    MenuStartowe
    Utworzenie okna menu startowego
    dodanie przycisków

---
+++ TicTacToe/TicTacToe/MenuWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)  // przycisk który przenosi nas do gry 
+        {
+            var GameWindow = new MainWindow();
+            GameWindow.Show();  // otwiera okno z gra
+            this.Close();     // gasi ekran startowy 
+        }
+    }
+}
