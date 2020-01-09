cf76c2fc89168097aff159b2097b6f61067fbd72
DarSpace <45310234+DarSpace@users.noreply.github.com>
2020-01-09 12:49:40 +0100

    Menu
    dodanie działania przycisków, dodanie styli

--- TicTacToe/TicTacToe/MenuWindow.xaml.cs
+++ TicTacToe/TicTacToe/MenuWindow.xaml.cs
@@ -27,8 +27,16 @@ namespace TicTacToe
         private void Button_Click(object sender, RoutedEventArgs e)  // przycisk który przenosi nas do gry 
         {
             var GameWindow = new MainWindow();
+            
             GameWindow.Show();  // otwiera okno z gra
             this.Close();     // gasi ekran startowy 
+
+          
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) // przycisk gaszący gre
+        {
+            this.Close();
         }
     }
 }
