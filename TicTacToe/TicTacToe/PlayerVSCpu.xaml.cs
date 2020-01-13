09c18ded102183b84878d28f1f27c1bb33900be0
habbababbai <50295184+habbababbai@users.noreply.github.com>
2020-01-13 18:22:30 +0100

    Okno gry z CPU
    Dodanie okna i logiki dla gry przeciwko komputerowi, drobne poprawki

--- TicTacToe/TicTacToe/PlayerVSCpu.xaml.cs
+++ TicTacToe/TicTacToe/PlayerVSCpu.xaml.cs
@@ -10,27 +10,32 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TicTacToeLibrary;
 
 namespace TicTacToe
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    
+    public partial class PlayerVSCpu : Window
     {
-        Game newGame;
-        List<Button> bList;
         
-        public MainWindow()
+        Game newGame;
+        
+        Button[,] btnList;
+
+        public PlayerVSCpu()
         {
             InitializeComponent();
             newGame = new Game();
-            bList = new List<Button>();
             WriteScore();
+            btnList = new Button[,] { {b0_0, b0_1, b0_2, b0_3, b0_4 },
+                                    {b1_0, b1_1, b1_2, b1_3, b1_4 },
+                                    {b2_0, b2_1, b2_2, b2_3, b2_4 },
+                                    {b3_0, b3_1, b3_2, b3_3, b3_4 },
+                                    {b4_0, b4_1, b4_2, b4_3, b4_4 } };
+            
         }
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (!newGame.isOver)
@@ -39,11 +44,13 @@ namespace TicTacToe
                 var column = Grid.GetColumn(button);
                 var row = Grid.GetRow(button);
                 newGame.SetBlock(row, column);
-                bList.Add(button);
-                if (newGame.gameBoard.arr[row, column] == CellState.X)
+                if (newGame.gameBoard.arr[row,column] == CellState.X)
                     button.Content = "X";
-                if (newGame.gameBoard.arr[row, column] == CellState.O)
-                    button.Content = "O";
+                if (!newGame.isOver)
+                {
+                    newGame.SetBlockCPU();
+                    btnList[newGame.lastXCPU, newGame.lastYCPU].Content = "O";
+                }
                 WriteScore();
             }
             else
@@ -53,22 +60,26 @@ namespace TicTacToe
                 WriteScore();
             }
         }
-        private void ClearBoard()
-        {
-            foreach (Button b in bList)
-            {
-                b.Content = "";
-            }
-        }
-        private void WriteScore()
-        {
-            txtbox.Text = $"Gracz 1 : {newGame.p1Score}     Gracz 2 : {newGame.p2Score}";
-        }
+        
         private void ReturnToMenu(object sender, RoutedEventArgs e)
         {
             var menu = new MenuWindow();
             menu.Show();
             this.Close();
+        }
+        private void ClearBoard()
+        {
+            for (int i = 0; i < btnList.GetLength(0); i++)
+            {
+                for (int j = 0; j < btnList.GetLength(1); j++)
+                {
+                    btnList[i, j].Content = "";
+                }
+            }
+        }
+        private void WriteScore()
+        {
+            txtbox.Text = $"Gracz : {newGame.p1Score}     Komputer : {newGame.p2Score}";
         }
     }
 }
--- TicTacToe/TicTacToeLibrary/Game.cs
+++ TicTacToe/TicTacToeLibrary/Game.cs
@@ -8,13 +8,43 @@ using System.Threading.Tasks;
 
 namespace TicTacToeLibrary
 {
+    /// <summary>
+    /// Klasa Game zajmująca się logiką gry
+    /// </summary>
     public class Game
     {
+        /// <summary>
+        /// player1Turn checks which player turn it is.
+        /// </summary>
         public bool player1Turn { get; private set; }
+        /// <summary>
+        /// gameBoard is object that holds information of every game block.
+        /// </summary>
         public Board gameBoard { get; private set; }
+        /// <summary>
+        /// isOver checks if game is over.
+        /// </summary>
         public bool isOver { get; private set; }
+        /// <summary>
+        /// Score of player one.
+        /// </summary>
         public int p1Score { get; private set; }
+        /// <summary>
+        /// Score of player2
+        /// </summary>
         public int p2Score { get; private set; }
+        /// <summary>
+        /// Last row on which cpu set its block.
+        /// </summary>
+        public int lastXCPU { get; private set; }
+        /// <summary>
+        /// Last column on which cpu set its block.
+        /// </summary>
+        public int lastYCPU { get; private set; }
+        private Random rnd = new Random();
+        /// <summary>
+        /// Constructor for Game class.
+        /// </summary>
         public Game()
         {
             gameBoard = new Board();
@@ -23,6 +53,11 @@ namespace TicTacToeLibrary
             p1Score = 0;
             p2Score = 0;
         }
+        /// <summary>
+        /// Method allowing to change specific block state.
+        /// </summary>
+        /// <param name="x">Row of block next to change.</param>
+        /// <param name="y">Column of block next to change</param>
         public void SetBlock(int x, int y)
         {
             if (gameBoard.arr[x,y] == CellState.Empty)
@@ -46,12 +81,49 @@ namespace TicTacToeLibrary
                     player1Turn = !player1Turn;
             }
         }
+        /// <summary>
+        /// Method setting block by CPU.
+        /// </summary>
+        public void SetBlockCPU()
+        {
+            if(!player1Turn)
+            {
+                int x = rnd.Next(0, 5);
+                int y = rnd.Next(0, 5);
+                if (gameBoard.arr[x, y] == CellState.Empty)
+                {
+                    gameBoard.arr[x, y] = CellState.O;
+                    lastXCPU = x;
+                    lastYCPU = y;
+                    if (CheckDraw())
+                        EndGame();
+                    if (CheckForLine())
+                    {
+                        EndGame();
+                        SetScore();
+                    }
+                }
+                else
+                {
+                    SetBlockCPU();
+                }
+            }
+            player1Turn = true;
+        }
+        /// <summary>
+        /// Method checks for line of same blocks.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckForLine()
         {
             if (CheckHorizontal() || CheckVertical() || CheckDiagonal())
                 return true;
             return false;
         }
+        /// <summary>
+        /// Method checks for vertical line.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckVertical()
         {
            for (int i = 0; i < 5; i++)
@@ -65,6 +137,10 @@ namespace TicTacToeLibrary
             }
             return false;
         }
+        /// <summary>
+        /// Method checks for horizontal line.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckHorizontal()
         {
             for (int i = 0; i < 5; i++)
@@ -78,6 +154,10 @@ namespace TicTacToeLibrary
             }
             return false;
         }
+        /// <summary>
+        /// Method checks for diagonal line.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckDiagonal()
         {
             if (gameBoard.arr[2,2] != CellState.Empty &&
@@ -94,6 +174,10 @@ namespace TicTacToeLibrary
                 return true;
             return false;
         }
+        /// <summary>
+        /// Method checks if every block is set as X or O.
+        /// </summary>
+        /// <returns></returns>
         private bool CheckDraw()
         {
             bool draw = true;
@@ -107,16 +191,25 @@ namespace TicTacToeLibrary
             }
             return draw;
         }
+        /// <summary>
+        /// Method ends game.
+        /// </summary>
         private void EndGame()
         {
             isOver = true;
         }
+        /// <summary>
+        /// Method resets gameBoard.
+        /// </summary>
         public void Reset()
         {
             gameBoard = new Board();
             isOver = false;
             player1Turn = true;
         }
+        /// <summary>
+        /// Method sets scores for players.
+        /// </summary>
         private void SetScore()
         {
             if (player1Turn)
