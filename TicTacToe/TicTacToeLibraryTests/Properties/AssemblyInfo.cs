ba85e0682fef0d70342b7bde2c9c876414929213
habbababbai <50295184+habbababbai@users.noreply.github.com>
2020-01-22 11:30:31 +0100

    Niewielkie poprawki

---
+++ TicTacToe/TicTacToeLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TicTacToeLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TicTacToeLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32068eff-7217-4ea6-9541-1ce1dd25a380")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- TicTacToe/TicTacToeTestt/TicTacToeTestt.cs
+++ TicTacToe/TicTacToeTestt/TicTacToeTestt.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TicTacToeLibrary;
 
+
 namespace TicTacToeTestt
 {
     [TestClass]
